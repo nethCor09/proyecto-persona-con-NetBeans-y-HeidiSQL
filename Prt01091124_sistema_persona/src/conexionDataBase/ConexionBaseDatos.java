a43b6be63bbfdd1d1b82e7554524b2569269690b
nethcor <nethcor9@gmail.com>
2024-11-09 18:08:44 -0400

    se establece conexion con la base de datos

---
+++ Prt01091124_sistema_persona/src/conexionDataBase/ConexionBaseDatos.java
@@ -0,0 +1,24 @@
+
+package conexionDataBase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConexionBaseDatos {
+    private String url = "jdbc:mysql://localhost:3306/sistema_persona";
+    private String userName = "root";
+    private String password = "";
+    
+    private Connection con;
+    
+    public ConexionBaseDatos() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, userName, password);
+            System.out.println("conexion exitosa");
+        }catch(Exception e) {
+            System.err.println("No se puedo establecer conexion con la Base de Datos. ERROR: "+e);
+        }
+    }
+    
+}
