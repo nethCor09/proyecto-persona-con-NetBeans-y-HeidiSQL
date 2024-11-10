9e171538042739c0990d991b3cb5e0b271987527
nethcor <nethcor9@gmail.com>
2024-11-10 02:24:08 -0400

    se implementa el metodo main

---
+++ Prt01091124_sistema_persona/src/ejecutar/GestionUsuarios.java
@@ -0,0 +1,14 @@
+
+package ejecutar;
+
+import interfazGrafica.Persona;
+
+public class GestionUsuarios {
+
+    public static void main(String args[]) {
+        
+        Persona nuevaPersona = new Persona();
+        nuevaPersona.setVisible(true);
+        nuevaPersona.setLocationRelativeTo(null);
+    }
+}
